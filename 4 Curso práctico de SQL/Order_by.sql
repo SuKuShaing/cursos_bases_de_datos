725be4cd79b0fae28efcc7aec1bc5c898c7eb2d1
Sebastian <sebastian.sanhueza.t@usach.cl>
2024-05-05 02:01:12 -0400

    Edu(Order By): ejemplos de Order By

---
+++ 4 Curso práctico de SQL/Order_by.sql
@@ -0,0 +1,14 @@
+SELECT *
+FROM tabla_diaria
+ORDER BY fecha;
+
+DESC -- Desc es para ordenar de manera descendente
+ASC -- Asc es para ordenar de manera ascendente
+
+SELECT *
+FROM tabla_diaria
+ORDER BY fecha ASC;
+
+SELECT *
+FROM tabla_diaria
+ORDER BY fecha DESC; 
