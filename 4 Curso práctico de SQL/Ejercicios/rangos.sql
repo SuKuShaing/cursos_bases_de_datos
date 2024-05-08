69d78fb4041d28390009b205ad52db318bacf0b8
Sebastian <sebastian.sanhueza.t@usach.cl>
2024-05-08 15:36:27 -0400

    Edu(rangos): se dan ejmplos de rangos

---
+++ 4 Curso práctico de SQL/Ejercicios/rangos.sql
@@ -0,0 +1,59 @@
+-- Manejo de rangos
+
+SELECT *
+FROM platzi.alumnos
+WHERE tutor_id IN (1, 2, 3, 4)
+ORDER BY tutor_id ASC;
+
+
+SELECT *
+FROM platzi.alumnos
+WHERE tutor_id BETWEEN 1 AND 10;
+
+
+SELECT int4range(1, 20) @> 3; -- El @> significa "contiene", devuelve un True
+-- Los rangos devulven [1, 20), es decir el 1 incluido en el rango y el 20 no incluido
+
+
+SELECT numrange(11.1, 22.2); -- Te da un rango de números "[11.1,22.2) '[ <- incluido', ') <- no incluido', por ende es el 11.1 incluido en el rango y el 22.2 no incluido " 
+
+
+SELECT * FROM generate_series(1, 20); -- Te muestra una lista con los número en la serie generada
+
+
+SELECT UPPER(int8range(15,25)); -- Te da el valor más bajo del rango
+
+
+SELECT LOWER(int8range(15,25)); -- Te da el valor más alto del rango
+
+
+SELECT int4range(10, 20) * int4range(15,25); -- Regresa la intersección (15,20)
+
+
+SELECT ISEMPTY(numrange(1,5)); -- Verifica sí es vacio
+
+
+SELECT *
+FROM platzi.alumnos
+WHERE int4range(10, 20) @> tutor_id
+ORDER BY tutor_id;
+-- Devuelve la tabla con los tutores dentro de ese rango
+
+
+-- Los tipos de rango que vienen en PostgreSQL son:
+-- -- int4range: Que trae un rango de enteros.
+
+-- -- int8range: Es un rango de enteros grandes.
+
+-- -- numrange: Es un rango numérico.
+
+-- -- tsrange: Es un rango del tipo timestamp pero sin la zona horaria.
+
+-- -- tstzrange: Es un rango del tipo timestamp con la zona horaria
+
+-- -- daterange: Es un rango del tipo fecha.
+
+-- Esos son los que podemos usar como selectores de rango dentro de PostgreSQL si les interesa conocer más Acá (https://www.postgresql.org/docs/9.2/rangetypes.html)
+
+
+-- Reto: Encontrar cruce de rangos
