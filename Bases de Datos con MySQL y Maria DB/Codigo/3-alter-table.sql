8296dc1669058bcf4f5be2dbd0d31d4a18725804
Sebastian <sebastian.sanhueza.t@usach.cl>
2024-10-25 13:56:29 -0300

    Se añaden descripciones al alter table

--- Bases de Datos con MySQL y Maria DB/Codigo/3-alter-table.sql
+++ Bases de Datos con MySQL y Maria DB/Codigo/3-alter-table.sql
@@ -1,6 +1,7 @@
 --Lo que enseño el profe
 USE metro_cdmx;
 
+-- Modifica las características de una columna y añade una llave primaria
 ALTER TABLE `stations`
 MODIFY `id` BIGINT(20) UNSIGNED NOT NULL AUTO_INCREMENT
 ADD PRIMARY KEY(id);
@@ -14,13 +15,18 @@ ADD PRIMARY KEY(id);
 -- estas son las modificaciones que hice yo
 USE metro_cdmx;
 
+-- Añade una llave foránea
 ALTER TABLE `trains`
 ADD CONSTRAINT `trains_line_id_foreign`
 FOREIGN KEY (`line_id`) REFERENCES `lines` (`id`);
 
-
+-- Modifica las características de una columna
 ALTER TABLE `station`
 MODIFY `id` BIGINT(20) UNSIGNED NOT NULL AUTO_INCREMENT;
 
-
+-- Cambia el nombre a la tabla
 ALTER TABLE `station` RENAME `stations`;
+
+-- Añade una columna
+ALTER TABLE `usuarios`
+ADD COLUMN `id_rol` INT(11) NOT NULL;
